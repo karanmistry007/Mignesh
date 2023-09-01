eb197bd98ff46c211d3352d7e423fce3cef3f5cf
karanmistry007git config --global user.email ksmistry007@gmail.com <ksmistry007@gmail.com>
2023-09-01 18:32:06 +0530

    now

--- src/js/client_slide.js
+++ src/js/client_slide.js
@@ -1,40 +1,36 @@
-// function clientSlider(){
-//     var slide1=document.querySelector(".main-slide").querySelector(".slide-1");
-//     var slide2=document.querySelector(".main-slide").querySelector(".slide-2");
-//     slide1.style.transform==="translateX(0%)"
-//     // console.log (slide1);
-//     // console.log (slide2);
+function clientSlider(){
+    var slide1=document.querySelector(".main-slide").querySelector(".slide-1");
+    var slide2=document.querySelector(".main-slide").querySelector(".slide-2");
+    slide1.style.transform==="translateX(0%)"
+    // console.log (slide1);
+    // console.log (slide2);
 
-//     function slideDisplay(){
-//         if (slide1.classList.contains("trans_left")){
+    function slideDisplay(){
+        if (slide2.classList.contains("trans_left")||(slide2.classList.contains("trans_right"))){
 
-//             slide1.classList.add("trans_right");
-//             slide1.classList.remove("trans_right");
-//             setTimeout(() => {
-//                 slide1.classList.remove("trans_left");
-//                 slide2.classList.add("trans_left");                
-//             }, 1000);
+            //for slide 2
 
+            //exit animation
 
+                // slide1.classList.remove("hidden");
 
-//             // slide1.style.transform="translateX(-100%)"
-//             // slide2.style.transform="translateX(0%)"
-//         } else {
-//             slide2.classList.add("trans_right");
-//             slide2.classList.remove("trans_right");
-//             setTimeout(() => {
-//                 slide1.classList.add("trans_left");
-//                 slide2.classList.remove("trans_left");
-                
-//             }, 1000);
+            // slide1.style.transform="translateX(-100%)"
+            // slide2.style.transform="translateX(0%)"
+        } else {
+            //for slide 1
 
-//             // slide2.style.transform="translateX(-100%)"
-//             // slide1.style.transform="translateX(0%)"
-//         }
-//     }
-//     setInterval(slideDisplay, 2000);
+            //exit animation
+
+                // slide2.classList.remove("hidden");
+            // slide2.classList.remove("trans_left");
+
+            // slide2.style.transform="translateX(-100%)"
+            // slide1.style.transform="translateX(0%)"
+        }
+    }
+    setInterval(slideDisplay, 3600);
 
 
-// }
+}
 
-// clientSlider();
+clientSlider();
