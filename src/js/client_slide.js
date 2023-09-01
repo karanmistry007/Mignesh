6612db74b7065f06ec61aa01784186c3b83a81eb
karanmistry007git config --global user.email ksmistry007@gmail.com <ksmistry007@gmail.com>
2023-09-01 16:15:00 +0530

    l

---
+++ src/js/client_slide.js
@@ -0,0 +1,40 @@
+// function clientSlider(){
+//     var slide1=document.querySelector(".main-slide").querySelector(".slide-1");
+//     var slide2=document.querySelector(".main-slide").querySelector(".slide-2");
+//     slide1.style.transform==="translateX(0%)"
+//     // console.log (slide1);
+//     // console.log (slide2);
+
+//     function slideDisplay(){
+//         if (slide1.classList.contains("trans_left")){
+
+//             slide1.classList.add("trans_right");
+//             slide1.classList.remove("trans_right");
+//             setTimeout(() => {
+//                 slide1.classList.remove("trans_left");
+//                 slide2.classList.add("trans_left");                
+//             }, 1000);
+
+
+
+//             // slide1.style.transform="translateX(-100%)"
+//             // slide2.style.transform="translateX(0%)"
+//         } else {
+//             slide2.classList.add("trans_right");
+//             slide2.classList.remove("trans_right");
+//             setTimeout(() => {
+//                 slide1.classList.add("trans_left");
+//                 slide2.classList.remove("trans_left");
+                
+//             }, 1000);
+
+//             // slide2.style.transform="translateX(-100%)"
+//             // slide1.style.transform="translateX(0%)"
+//         }
+//     }
+//     setInterval(slideDisplay, 2000);
+
+
+// }
+
+// clientSlider();
